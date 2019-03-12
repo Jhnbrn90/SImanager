b73ebfb2459b30a3f4cbc54d229a3593860d5805
John Braun <jbraunnl@gmail.com>
2019-03-12 08:53:22 +0100

    Remove broadcast specific logic

--- app/Events/UserWasCreated.php
+++ app/Events/UserWasCreated.php
@@ -3,17 +3,12 @@
 namespace App\Events;
 
 use App\User;
-use Illuminate\Broadcasting\Channel;
 use Illuminate\Queue\SerializesModels;
-use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Foundation\Events\Dispatchable;
-use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 
 class UserWasCreated
 {
-    use Dispatchable, InteractsWithSockets, SerializesModels;
+    use Dispatchable, SerializesModels;
 
     public $user;
 
@@ -25,15 +20,5 @@ class UserWasCreated
     public function __construct(User $user)
     {
         $this->user = $user;
-    }
-
-    /**
-     * Get the channels the event should broadcast on.
-     *
-     * @return \Illuminate\Broadcasting\Channel|array
-     */
-    public function broadcastOn()
-    {
-        return new PrivateChannel('channel-name');
     }
 }
