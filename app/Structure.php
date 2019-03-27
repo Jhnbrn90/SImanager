74b8c9c9aa526f8ad9df08c7761a574e333d988e
John Braun <jbraunnl@gmail.com>
2019-03-27 20:36:19 +0100

    Remove obsolete code

--- app/Structure.php
+++ app/Structure.php
@@ -28,55 +28,6 @@ class Structure extends Model
         $query->where($attributes);
     }
 
-    public function getSVGPathAttribute()
-    {
-        if (! $this->molfile) {
-            return 'storage/svg/unknown.svg';
-        }
-
-        return "storage/database/svg/{$this->id}.svg?".time();
-    }
-
-    public function svgPath()
-    {
-        if (! $this->molfile) {
-            return 'storage/svg/unknown.svg';
-        }
-
-        return "storage/database/svg/{$this->id}.svg?".time();
-    }
-
-    public function getpathToSVGAttribute()
-    {
-        return storage_path()."/app/public/database/svg/{$this->chemical_id}.svg";
-    }
-
-    public function getpathToMolfileAttribute()
-    {
-        return storage_path()."/app/public/database/molfiles/{$this->chemical_id}.mol";
-    }
-
-    public function toMolfile()
-    {
-        if ($this->molfile[0] == ' ' || $this->molfile[0] == 'J') {
-            // insert newline if the first character of the first line is a space or J (from JSDRAW)
-            $this->molfile = "\r\n".$this->molfile;
-        }
-
-        Storage::put("public/database/molfiles/{$this->id}.mol", $this->molfile);
-
-        return $this;
-    }
-
-    public function toSVG()
-    {
-        if (! $this->molfile) {
-            return;
-        }
-
-        $output = shell_exec('echo "'.$this->molfile.'" | /usr/local/bin/mol2svg --bgcolor=white --color=colors.conf - > '.$this->pathToSVG);
-    }
-
     public function saveSvg()
     {
         if (! $this->molfile) {
